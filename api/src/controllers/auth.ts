502d2f6db701515ac74e6c02291d81e9dd611dcd
Borges dev <leosull50@hotmail.com>
2025-04-11 18:17:09 -0300

    feat: gerando token JWT

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -1,5 +1,33 @@
 import { RequestHandler } from "express";
+import { z } from "zod";
+import { createUser } from "../services/user";
+import { createToken } from "../services/auth";
 
 export const signup: RequestHandler = async (req, res) => {
-    
+    const schema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string()
+    })
+    const data  = schema.safeParse(req.body)
+    if (!data.success) {
+        res.json({error: data.error?.flatten().fieldErrors})
+        return;
+    }
+    const newUser = await createUser(data.data)
+
+    if (!newUser) {
+        res.json({error: "Erro ao criar usuário"});
+        return
+    }
+    const token = createToken(newUser)
+
+    res.status(201).json({
+        user: {
+            id: newUser.id,
+            name: newUser.name,
+            email: newUser.email
+        },
+        token
+    })
 }
